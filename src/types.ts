71c436c754ac784ae9d906716cfbef46b1b2cfa0
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-02-11 13:13:07 +0100

    not using

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,24 @@
-// no change has been made so far; we keep 'year' as a 
-// string for easy comparison in the query.
-// We can change year as a number; we can change it 
-// later + adjust the rest.
+// // no change has been made so far; we keep 'year' as a 
+// // string for easy comparison in the query.
+// // We can change year as a number; we can change it 
+// // later + adjust the rest.
 
-export type Book = {
-  id: string;
-  title: string;
-  author: string;
-  year: string;
-};
+// export type Book = {
+//   id: string;
+//   title: string;
+//   author: string;
+//   year: string;
+// };
 
-export type User = {
-  id: string;
-  name: string;
-  email: string;
-};
+// export type User = {
+//   id: string;
+//   name: string;
+//   email: string;
+// };
 
-export type Loan = {
-  user_id: string;
-  book_id: string;
-  due_date: string;
-  created_at: string;
-};
+// export type Loan = {
+//   user_id: string;
+//   book_id: string;
+//   due_date: string;
+//   created_at: string;
+// };
