5def5a47aca491b0791352d2f749c88b4322b5c5
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-02-11 13:20:49 +0100

    adding new routes to the server

--- src/server.ts
+++ src/server.ts
@@ -6,26 +6,45 @@
 // Tester imports buildServer().
 
 import Fastify from "fastify";
-import { loadBooksOnce } from "./data/books-loader";
-import routes from "./routes";
+// import { loadBooksOnce } from "./data/books-loader";
+// import routes from "./routes";
+import { registerErrorHandler } from "./errors/error-handler";
+import { booksRoutes } from "./routes/books.routes";
+import { customersRoutes } from "./routes/customers.routes";
+import { ordersRoutes } from "./routes/orders.routes";
 
-export async function buildServer() {
-    const fastifyServer = Fastify({
-        logger: false, // keeping tests silent (can be turned on later)
-        ajv: {
-            customOptions: {
-                allErrors: true,
-                coerceTypes: false,
-                removeAdditional: false,
-            },
-        },
-    });
 
-    // load books into store before routes
-    await loadBooksOnce();
+// export async function buildServer() {
+//     const fastifyServer = Fastify({
+//         logger: false, // keeping tests silent (can be turned on later)
+//         ajv: {
+//             customOptions: {
+//                 allErrors: true,
+//                 coerceTypes: false,
+//                 removeAdditional: false,
+//             },
+//         },
+//     });
 
-    // register routes - MUST await this
-    await fastifyServer.register(routes);
+//     // load books into store before routes
+//     await loadBooksOnce();
 
-    return fastifyServer;
+//     // register routes - MUST await this
+//     await fastifyServer.register(routes);
+
+//     return fastifyServer;
+// }
+
+export function buildServer() {
+  const app = Fastify({ logger: true });
+
+  registerErrorHandler(app);
+
+  app.get("/health", async () => ({ ok: true }));
+
+  app.register(booksRoutes);
+  app.register(customersRoutes);
+  app.register(ordersRoutes);
+
+  return app;
 }
