eb6e2d62ac7425b541cef9495fdb2b44da897e5b
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-02-13 00:58:50 +0100

    updated

--- src/errors/error-handler.ts
+++ src/errors/error-handler.ts
@@ -16,9 +16,16 @@ export function registerErrorHandler(app: FastifyInstance) {
     const anyErr = err as any;
     const statusCode = typeof anyErr.statusCode === "number" ? anyErr.statusCode : 500;
 
+    // return reply.status(statusCode).send({
+    //   error: statusCode === 500 ? "Internal Server Error" : err.message,
+    //   details: statusCode === 500 ? null : anyErr.validation ?? null,
+    // });
+
     return reply.status(statusCode).send({
-      error: statusCode === 500 ? "Internal Server Error" : err.message,
-      details: statusCode === 500 ? null : anyErr.validation ?? null,
+        error: err.message,
+        details: anyErr.validation ?? null,
     });
+
+    
   });
 }
