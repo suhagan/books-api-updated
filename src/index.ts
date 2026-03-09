7e0140bebaabf14d414ecc89a9e29186a589b2ad
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-03-09 01:40:26 +0100

    refactored the server startup to be properly asynchronous

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,44 @@
-// import fastify from "fastify";
-// import routes from "./routes";
+// // import fastify from "fastify";
+// // import routes from "./routes";
 
-// const fastifyServer = fastify({
-//   logger: true,
-//   ajv: {
-//     customOptions: {
-//       allErrors: true,
-//       coerceTypes: false,
-//       removeAdditional: false,
-//     },
-//   },
+// // const fastifyServer = fastify({
+// //   logger: true,
+// //   ajv: {
+// //     customOptions: {
+// //       allErrors: true,
+// //       coerceTypes: false,
+// //       removeAdditional: false,
+// //     },
+// //   },
+// // });
+
+// import { buildServer } from "./server";
+
+// // const start = async () => {
+// //   const fastifyServer = await buildServer();
+// //   // await fastifyServer.register(routes);
+
+// //   await fastifyServer.listen({ host: "0.0.0.0", port: 3000 });
+
+// //   console.log("Server is listening on http://localhost:3000!");
+// // };
+
+// // start();
+
+// const app = buildServer();
+
+// app.listen({ port: 3000, host: "0.0.0.0" }).then(() => {
+//   console.log("Server running on http://localhost:3000");
 // });
 
 import { buildServer } from "./server";
 
-// const start = async () => {
-//   const fastifyServer = await buildServer();
-//   // await fastifyServer.register(routes);
+async function start() {
+  const app = await buildServer();
 
-//   await fastifyServer.listen({ host: "0.0.0.0", port: 3000 });
+  await app.listen({ port: 3000, host: "0.0.0.0" });
 
-//   console.log("Server is listening on http://localhost:3000!");
-// };
-
-// start();
-
-const app = buildServer();
-
-app.listen({ port: 3000, host: "0.0.0.0" }).then(() => {
   console.log("Server running on http://localhost:3000");
-});
+}
+
+start();
