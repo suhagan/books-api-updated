48baee651fc062563b970ff7f31392148c3adfe3
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-03-10 03:51:47 +0100

    POST & DELETE are protected

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -1,12 +1,30 @@
 import type { FastifyInstance } from "fastify";
 import { CustomersController } from "../controllers/customers.controller";
 import { createCustomerBodySchema, customerIdParamsSchema } from "../schemas/customers.schemas";
+import { verifyJwt } from "../auth/auth-middleware";
 
 export async function customersRoutes(app: FastifyInstance) {
   const c = new CustomersController();
 
   app.get("/customers", c.list);
   app.get("/customers/:id", { schema: { params: customerIdParamsSchema } }, c.get);
-  app.post("/customers", { schema: { body: createCustomerBodySchema } }, c.create);
-  app.delete("/customers/:id", { schema: { params: customerIdParamsSchema } }, c.remove);
+  //app.post("/customers", { schema: { body: createCustomerBodySchema } }, c.create);
+  app.post(
+    "/customers",
+    {
+      preHandler: verifyJwt,
+      schema: { body: createCustomerBodySchema },
+    },
+    c.create
+  );
+
+  // app.delete("/customers/:id", { schema: { params: customerIdParamsSchema } }, c.remove);
+  app.delete(
+    "/customers/:id",
+    {
+      preHandler: verifyJwt,
+      schema: { params: customerIdParamsSchema },
+    },
+    c.remove
+  );
 }
