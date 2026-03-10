87b6d6d51a8670a5fb418853872f8f66fa3b97df
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-03-10 03:50:44 +0100

    POST route is protected.

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,12 +1,21 @@
 import type { FastifyInstance } from "fastify";
 import { OrdersController } from "../controllers/orders.controller";
 import { createOrderBodySchema, customerIdParamsSchemaForOrders } from "../schemas/orders.schemas";
+import { verifyJwt } from "../auth/auth-middleware";
 
 export async function ordersRoutes(app: FastifyInstance) {
   const c = new OrdersController();
 
   app.get("/orders", c.list);
-  app.post("/orders", { schema: { body: createOrderBodySchema } }, c.create);
+  // app.post("/orders", { schema: { body: createOrderBodySchema } }, c.create);
+  app.post(
+    "/orders",
+    {
+      preHandler: verifyJwt,
+      schema: { body: createOrderBodySchema },
+    },
+    c.create
+  );
 
   // "Get all existing orders for a specific customer"
   app.get(
