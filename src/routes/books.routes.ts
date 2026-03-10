7108d81657724ca509c67e28e78eea1b24846932
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2026-03-10 03:49:26 +0100

    POST, PUT & DELETE routes are now protected.

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -6,6 +6,7 @@ import {
   createBookBodySchema,
   updateBookBodySchema,
 } from "../schemas/books.schemas";
+import { verifyJwt } from "../auth/auth-middleware";
 
 export async function booksRoutes(app: FastifyInstance) {
   const c = new BooksController();
@@ -13,8 +14,36 @@ export async function booksRoutes(app: FastifyInstance) {
   app.get("/books", { schema: { querystring: booksQuerySchema } }, c.list);
   app.get("/books/:id", { schema: { params: bookIdParamsSchema } }, c.get);
 
-  app.post("/books", { schema: { body: createBookBodySchema } }, c.create);
-  app.put("/books/:id", { schema: { params: bookIdParamsSchema, body: updateBookBodySchema } }, c.update);
+  // app.post("/books", { schema: { body: createBookBodySchema } }, c.create);
+  app.post(
+    "/books",
+    {
+      preHandler: verifyJwt,
+      schema: { body: createBookBodySchema },
+    },
+    c.create
+  );
 
-  app.delete("/books/:id", { schema: { params: bookIdParamsSchema } }, c.remove);
+  // app.put("/books/:id", { schema: { params: bookIdParamsSchema, body: updateBookBodySchema } }, c.update);
+  app.put(
+    "/books/:id",
+    {
+      preHandler: verifyJwt,
+      schema: {
+        params: bookIdParamsSchema,
+        body: updateBookBodySchema,
+      },
+    },
+    c.update
+  );
+
+  // app.delete("/books/:id", { schema: { params: bookIdParamsSchema } }, c.remove);
+  app.delete(
+    "/books/:id",
+    {
+      preHandler: verifyJwt,
+      schema: { params: bookIdParamsSchema },
+    },
+    c.remove
+  );
 }
